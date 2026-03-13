82fa82ebb568510a9ce6a4c94af59aec3f0e8d5b
FastApi Simo <fastapisimo@simo-HP-EliteBook-745-G2>
2026-03-13 10:32:42 +0000

    add /magic/json/<dim:int>

--- server.py
+++ server.py
@@ -20,4 +20,22 @@ def index(dim):
     message = {"magic":square.tolist()}
     return {'message':message, 'n':dim}
 
+@route('/magic/json/<dim:int>')
+def index(dim):
+
+    if dim % 2 == 0 :
+        return "Doesnt work for even"
+
+    square = generate_magic_square_odd(dim)
+    print(f"Magic Square of order {dim}:\n{square}")
+
+    # The magic constant (sum of rows/cols/diagonals) can be verified:
+    magic_constant = dim * (dim**2 + 1) // 2
+    print(f"\nMagic Constant: {magic_constant}")
+
+    #return template('{{square}}!', square=square)
+    message = {"magic":square.tolist()}
+    return {'message':message, 'n':dim}
+
+
 run(host='localhost', port=8080)
